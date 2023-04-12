493de05182805f0cad08a724820339c477bfb619
Saidazam <saidolmovsaid@gmail.com>
2023-04-12 02:55:54 -0700

    changes

--- lib/screens/send_report_screen/send_report_screen.dart
+++ lib/screens/send_report_screen/send_report_screen.dart
@@ -78,6 +78,11 @@ class SendReportScreen extends StatelessWidget {
                   context: context,
                   builder: (context) => AlertDialog(
                     title: Text(value.error),
+                    actions: [
+                      TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text("Ok"))
+                    ],
                   ),
                 );
               }
